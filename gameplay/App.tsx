aa7b953fbd89c3d5be73efe1d85803014d958a39
phcbarros <paulo.barros@outlook.com.br>
2021-06-20 22:13:24 -0300

    [Mission: React Native] Aula 01 – Liftoff

---
+++ gameplay/App.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import { Text, View } from 'react-native';
+import {SignIn} from './src/screens/Signin';
+
+export default function App() {
+  return (
+    <SignIn />
+  );
+}
---
+++ gameplay/src/components/ButtonIcon/styles.ts
@@ -0,0 +1,31 @@
+import { StyleSheet } from 'react-native'
+import { theme } from '../../global/styles/theme'
+
+export const styles = StyleSheet.create({
+  container: {
+    width: '100%',
+    height: 56,
+    backgroundColor: theme.colors.primary,
+    borderRadius: 8,
+    flexDirection: 'row',
+    alignItems: 'center',
+  },
+  iconWrapper: {
+    width: 56,
+    height: 56,
+    borderRightColor: theme.colors.line,
+    borderRightWidth: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  icon: {
+    height: 18,
+    width: 24,
+  },
+  title: {
+    flex: 1,
+    color: theme.colors.heading,
+    fontSize: 15,
+    textAlign: 'center',
+  },
+})
